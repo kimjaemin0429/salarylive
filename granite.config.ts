946e5b9ccd00c1128fe9ca6c5fa94e99ef3c3907
kimjaemin0429 <89975964+kimjaemin0429@users.noreply.github.com>
2026-06-13 16:43:55 +0900

    Update granite.config.ts

--- granite.config.ts
+++ granite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 export default defineConfig({
   appName: 'salarylive',
   brand: {
-    displayName: '연봉 LIVE 카운트',
+    displayName: '머니카운트',
     primaryColor: '#FF6B9D',
-    icon: 'https://static.toss.im/appsintoss/30821/7c447c56-33a9-4cfc-a497-ec18bc66b7aa.png', // 콘솔 앱 정보 > 아이콘 우클릭 > 링크 복사
+    icon: 'https://static.toss.im/appsintoss/30821/a771645b-20ec-4fe2-aa46-c5c7918d12ea.png', // 콘솔 앱 정보 > 아이콘 우클릭 > 링크 복사
   },
   web: {
     host: 'localhost',
